a3882692a740956edcd6f199e73de8c1e8300683
Geoff Hodgson <geoffroh@gmail.com>
2020-12-09 07:36:26 +1100

    Refactor day 7

--- 2020/7.rb
+++ 2020/7.rb
@@ -1,71 +1,53 @@
 # https://adventofcode.com/2020/day/7
 
 class AocDay7
-  attr_reader :input, :contained_by, :contents
+  attr_reader :input, :bag_structure
 
   def initialize
     @input = File.read("2020/input/7").split("\n")
-    # Formats the input in a {bag_type (contained_by) bag_type} hash so that we can see what a bag is contained in
-    @contained_by = normalise_container_bags
-    # Formats the input in a {bag_type (contains) {bag type: a, number: x}} hash,
-    # so that we can see what/how many bags are contained in a given bag type
-    @contents = normalise_bag_contents
+    @bag_data = parse_input
   end
 
   # How many different bags can eventually contain a golden bag
   def part_a(bag_type)
-    containers_for(bag_type).flatten.uniq.size
+    containers_for(bag_type.sub(' bag', '')).flatten.uniq.size
   end
 
   # How many bags must the given type contain
   def part_b(bag_type)
-    contents_for(bag_type)
+    contents_for(bag_type.sub(' bag', ''))
   end
 
   private
 
   # Recursively find the bags that can contain the given type
   def containers_for(bag_type)
-    Array(contained_by[bag_type]).map do |container_bag_type|
+    @bag_data[bag_type]["contained_by"].map do |container_bag_type|
       [container_bag_type] + containers_for(container_bag_type)
     end
   end
 
   # Recursively find the number of bags contained by the given type
   def contents_for(bag_type)
-    Array(contents[bag_type]).sum do |number_and_type|
-      number_and_type[:number] + number_and_type[:number] * contents_for(number_and_type[:type])
+    @bag_data[bag_type]["contains"].sum do |contents_type, contents_count|
+      contents_count + contents_count * contents_for(contents_type)
     end
   end
 
-  # Turns the input to a hash of bag colour => bag colours they are contained in
-  def normalise_container_bags
+  # Structures the input to a hash:
+  # {"a" => {"contains" => {"b" => 2, "c" => 3}}, "contained_by" => ["d"}}
+  def parse_input
     input.reduce({}) do |acc, row|
-      container, contents = row.split("s contain")
-       contents.split(', ').map do |c|
-         singular_content = c.gsub(/\d/, '').sub('bags', 'bag').sub('.', '').strip
-         acc[singular_content]||=[]
-         acc[singular_content] += [container]
-       end
-       acc
-     end
-  end
+      key, data = row.split(" bags contain ")
+      acc[key] ||= {"contains" => {}, "contained_by" => []}
 
-
-  def normalise_bag_contents
-    input.reduce({}) do |acc, row|
-      container, contents = row.split("s contain")
-      acc[container] =
-        contents.split(', ').reduce([]) do |b, c|
-          break if c.strip == "no other bags."
-
-          b += [
-            {
-              number: /\d/.match(c)[0].to_i,
-              type: c.gsub(/\d/, '').sub('bags', 'bag').sub('.', '').strip
-            }
-          ]
-        end
+      data.scan(/(\d) ([a-z]+ [a-z]+) bag/)
+          .map do |dd|
+            num, type = dd
+            acc[key]["contains"].merge!(type => num.to_i)
+            acc[type] ||= {"contains" => {}, "contained_by" => []}
+            acc[type]["contained_by"] += [key]
+          end
       acc
     end
   end
